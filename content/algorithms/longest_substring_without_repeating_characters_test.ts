4f8efa6423eb95d01844116b4096210009be10ab
Owen Young <theowenyoung@gmail.com>
2022-05-08 10:17:36 +0800

    feat: format code

--- content/algorithms/longest_substring_without_repeating_characters_test.ts
+++ content/algorithms/longest_substring_without_repeating_characters_test.ts
@@ -1,6 +1,23 @@
 import { assertEquals } from "https://deno.land/std@0.138.0/testing/asserts.ts";
-import { lengthOfLongestSubstring } from "./longest_substring_without_repeating_characters.ts";
+function lengthOfLongestSubstring(s: string): number {
+  const arr = s.split("https://deno.land/std@0.138.0/testing/asserts.ts");
+  let longest = 0;
+  const set: Set<string> = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    set.add(arr[i]);
+    let tryIndex = i + 1;
+    while (arr[tryIndex] && !set.has(arr[tryIndex])) {
+      set.add(arr[tryIndex]);
+      tryIndex++;
+    }
+    if (set.size > longest) {
+      longest = set.size;
+    }
+    set.clear();
+  }
 
+  return longest;
+}
 Deno.test("length of longest substring test 1", () => {
   const s = "abcabcbb";
   const result = lengthOfLongestSubstring(s);
