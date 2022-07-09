6a08ecae5bafaf1f5362d96de7835f520d2c1156
Owen Young <theowenyoung@gmail.com>
2022-07-10 00:00:32 +0800

    refactor build

--- Makefile
+++ Makefile
@@ -1,15 +1,11 @@
-.Phony: serve build build-linux test webmention send dev-webmention win
+.Phony: serve build test webmention send dev-webmention win
 
 serve:
 	./bin/zola serve
 
-win:
-	./bin/win-zola.exe serve
 build:
 	./bin/zola build
 
-build-linux:
-	./bin/linux-zola build
 
 test:
 	deno test
@@ -20,3 +16,7 @@ send:
 	deno run -A https://deno.land/x/denoflow/cli.ts run send-webmention.yml
 dev-webmention:
 	deno run -A ../denoflow/denoflow/cli.ts run --force
+
+.Phony: install
+install:
+	./scripts/install_zola.sh
---
+++ scripts/install_zola.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd );
+BIN_DIR="$SCRIPT_DIR/../bin"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  cd -- /tmp
+  curl -OL https://github.com/theowenyoung/zola/releases/latest/download/zola-x86_64-unknown-linux-gnu.tar.gz
+  tar -xf /tmp/zola-x86_64-unknown-linux-gnu.tar.gz -C $BIN_DIR
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+# Mac OSX
+  cd -- /tmp/
+  curl -OL https://github.com/theowenyoung/zola/releases/latest/download/zola-x86_64-apple-darwin.zip
+  unzip -o /tmp/zola-x86_64-apple-darwin.zip -d $BIN_DIR
+fi;
+
+echo Install Success.
+echo Run \`make serve\` to serve local
+
+echo Run \`make build\` to build to public folder
