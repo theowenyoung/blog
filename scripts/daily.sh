e970776f34ce7f924abb21563bf3ff404800214b
Owen Young <theowenyoung@gmail.com>
2022-09-12 14:12:38 +0800

    add daily log

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./scripts/common.sh
 template_path="./scripts/templates/daily.md.tmpl"
-target_path="./content/blog/journals/$(date +%Y-%m-%d).md"
+target_path="./content/blog/journals/$(date +%Y-%m-%d).en.md"
 
 template $template_path $target_path;
-code $target_path;
+kakproject $target_path;
