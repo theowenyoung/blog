0c4f7be7d380e66679e1d9426879cc2b5a73231b
Owen Young <theowenyoung@gmail.com>
2022-09-14 20:34:34 +0800

    add daily

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./scripts/common.sh
 template_path="./scripts/templates/daily.md.tmpl"
-target_path="./content/blog/journals/$(date +%Y-%m-%d).en.md"
+target_path="./content/blog/journals/$(date +%Y-%m-%d).md"
 
 template $template_path $target_path;
 kakproject $target_path;
