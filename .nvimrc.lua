ce2a3b9186a9f54e5155def3e66477fd0cf57ce2
Owen Young <theowenyoung@gmail.com>
2022-09-25 00:00:28 +0800

    add local nvim config

---
+++ .nvimrc.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n','<Leader>da',':exe "e" system("./scripts/daily.sh") <CR>')
+vim.keymap.set('n','<Leader>no',':exe "e" system("./scripts/notes.sh") <CR>')
+vim.keymap.set('n','<Leader>bo',':exe "e" system("./scripts/book.sh") <CR>')
+vim.keymap.set('n','<Leader>ra',':exe "e" system("./scripts/random.sh") <CR>')
