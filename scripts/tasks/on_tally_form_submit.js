bceffedacd5b7e3c2d60d6d00f0ea75244b7d5b8
Owen Young <theowenyoung@gmail.com>
2023-03-28 21:36:39 +0800

    update task

--- scripts/tasks/on_tally_form_submit.js
+++ scripts/tasks/on_tally_form_submit.js
@@ -4,9 +4,17 @@ export async function onTallyFormSubmit(body) {
   const { formName, fields } = data;
   let text = `新回复 from: ${formName}:\n`;
 
-  for (const field of fields) {
-    text += `${field.label}: ${field.value}\n`;
+  let value = "";
+  if (typeof fields.value === "string") {
+    value = fields.value;
+  } else if (typeof fields.value === "object" && fields.value.url) {
+    value = fields.value.url;
   }
+  for (const field of fields) {
+    text += `${field.label}: ${value}\n`;
+  }
+
+  text += `\n主页地址:  https://tally.so/forms/${data.formId}/submissions`;
 
   // send notice
   await sendNotice({
