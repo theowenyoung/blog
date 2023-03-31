18dd4b1268ed71445d5a48882f88b5ca170c5c0d
Owen Young <theowenyoung@gmail.com>
2023-04-01 03:49:58 +0800

    update notice

--- scripts/tasks/send_notice.ts
+++ scripts/tasks/send_notice.ts
@@ -54,6 +54,8 @@ export async function sendNotice(options: NoticeOptions) {
     body: raw,
     redirect: "follow",
   };
+  console.log("url", url);
+  console.log("requestOptions", JSON.stringify(requestOptions, null, 2));
 
   await request(
     url,
