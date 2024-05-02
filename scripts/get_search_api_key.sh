0a998c4f584253589df534f515ce0aade8a14543
Owen Young <theowenyoung@users.noreply.github.com>
2024-05-03 02:39:03 +0800

    update key

--- scripts/get_search_api_key.sh
+++ scripts/get_search_api_key.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 curl \
-    -X GET 'https://meili.owenyoung.com/keys' \
-    -H "Authorization: Bearer $TEMP_MEILISEARCH_API_KEY" |
-    json_pp
+	-X GET 'https://meilisearch.owenyoung.com/keys' \
+	-H "Authorization: Bearer $TEMP_MEILISEARCH_API_KEY" |
+	json_pp
